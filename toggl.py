9b21372348a4e6245c0c9d8f5cf46d9113aa948f
Nick Loadholtes <nick@ironboundsoftware.com>
2011-12-28 22:25:25 -0500

    stubbing in the class and main method

--- toggl.py
+++ toggl.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python
+
+
+class Toggl:
+    def send(req):
+        """
+        Sends requests to toggl
+        """
+        pass
+
+
+if __name__ == "__main__":
+    t = Toggl()
+    t.send()
