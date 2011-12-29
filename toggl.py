d79aa77ea6699b86f8ab0bee467263cdd4a9f57e
Nick Loadholtes <nick@ironboundsoftware.com>
2011-12-28 22:34:41 -0500

    Fleshing things out

--- toggl.py
+++ toggl.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
+import simplejason, urllib2, base64
 
 class Toggl:
-    def send(req):
+    URL = 'https://www.toggl.com/api/'
+    API_VERSION = 'v6'
+
+    def __init__(self, version='v6'):
+        API_VERSION = version
+
+    def send(self, req):
         """
         Sends requests to toggl
         """
-        pass
+        url = URL + API_VERSION + '/' + req
+        request = urllib2.Request(url, None)
+        request.add_header("Content-type", "application/json")
+
+        res = urllib2.urlopen(request)
+        result = simplejson.load(res)
 
 
 if __name__ == "__main__":
