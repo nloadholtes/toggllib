ba6067d6bce676b2edecdad95c012b6657c8857f
Nick Loadholtes <nick@ironboundsoftware.com>
2011-12-28 22:40:58 -0500

    Adding in the authorization

--- toggl.py
+++ toggl.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 
 import simplejason, urllib2, base64
 
@@ -6,21 +5,25 @@ class Toggl:
     URL = 'https://www.toggl.com/api/'
     API_VERSION = 'v6'
 
-    def __init__(self, version='v6'):
-        API_VERSION = version
+    def __init__(self, api_token, api_version='v6'):
+        API_VERSION = api_version
+        self.api_token = api_token
 
     def send(self, req):
         """
         Sends requests to toggl
         """
-        url = URL + API_VERSION + '/' + req
+        url = self.URL + self.API_VERSION + '/' + req
         request = urllib2.Request(url, None)
-        request.add_header("Content-type", "application/json")
+        request.add_header('Content-type', 'application/json')
+        authstr = base64.encodestring('%s:%s' % (self.api_token, 'api_token')).replace('\n', '')
+        request.add_header('Authorization', authstr)
 
         res = urllib2.urlopen(request)
         result = simplejson.load(res)
+        print(results)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     t = Toggl()
     t.send()
